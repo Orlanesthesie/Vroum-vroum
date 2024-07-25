9f46312c39e8974187d3b1dec8766ca83ce2ba04
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-07-25 16:50:53 +0200

    push jorge filament admin

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('lastname');
-            $table->string('firstname');
+            $table->string('lastname')->nullable();
+            $table->string('firstname')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role')->default('user');
