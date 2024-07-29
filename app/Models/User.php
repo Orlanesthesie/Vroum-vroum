f8a2b9bf6a95b0fe4ee7d6657746a50103e171f0
JorgeRobles1993 <jorgejoanrobles93@gmail.com>
2024-07-29 08:53:26 +0200

    filament admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,19 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-/**
- * @OA\Schema(
- *     schema="User",
- *     type="object",
- *     required={"id", "lastname", "firstname", "email"},
- *     @OA\Property(property="id", type="integer", description="ID of the user"),
- *     @OA\Property(property="lastname", type="string", description="Last name of the user"),
- *     @OA\Property(property="firstname", type="string", description="First name of the user"),
- *     @OA\Property(property="email", type="string", format="email", description="Email of the user"),
- *     @OA\Property(property="avatar", type="string", description="Avatar URL of the user", nullable=true),
- * )
- */
-
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
@@ -55,5 +42,15 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getNameAttribute()
+    {
+        return "{$this->firstname} {$this->lastname}";
     }
 }
